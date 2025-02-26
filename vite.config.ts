7558dc5617e6566231067adb6822c0723b39874a
dyhwangbo <dukyoon.hwangbo@nhnad.com>
2025-02-26 10:55:47 +0900

    보일러 플레이트 코드 푸쉬

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+
+export default defineConfig({
+    plugins: [
+        react()
+    ],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
+    server: {
+        port : 3030,
+        open : true
+
+    }
+});
